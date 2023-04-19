628f034922075f5e9ccd24bf64c4c27fddbe2f63
pavel281185 <pavelkvasnicka@gmail.com>
2023-04-19 14:03:30 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args) {
+        // expression = operands & operators
+        // operands = values, variables, numbers, quantity
+        // operators = 1 - * / %
+
+        int friends = 10;
+
+        friends = friends + 1;
+        System.out.println(friends);
+
+        friends = friends * 2;
+        System.out.println(friends);
+
+        friends = friends / 2;
+        System.out.println(friends);
+
+        friends = friends % 3;
+        System.out.println(friends);
+
+        friends++;
+        System.out.println(friends);
+
+        friends--;
+        System.out.println(friends);
+    }
+}
